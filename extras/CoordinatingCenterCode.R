c95c76378ae1747474302ef8f4e008ea8aaf33fb
schuemie <schuemie@ohdsi.org>
2020-10-17 02:17:04 -0400

    Updating coordinating center script

--- extras/CoordinatingCenterCode.R
+++ extras/CoordinatingCenterCode.R
@@ -6,7 +6,7 @@ localFolder <- "s:/PhenotypeLibraryResults"
 # dir.create(localFolder)
 
 # Download files from SFTP server -----------------------------------------------------------------
-connection <- sftpConnect(privateKeyFileName = "c:/home/keyfiles/study-coordinator-pldiag",
+connection <- sftpConnect(privateKeyFileName = "c:/home/keyfiles/study-coordinator-pldiag.dat",
                           userName = "study-coordinator-pldiag")
 
 # sftpMkdir(connection, "phenotypeLibrary")
@@ -14,6 +14,9 @@ connection <- sftpConnect(privateKeyFileName = "c:/home/keyfiles/study-coordinat
 sftpCd(connection, "phenotypeLibrary")
 files <- sftpLs(connection)
 files
+
+sftpCd(connection, "HERACharacterization")
+sftpCd(connection, "StudyResults")
 
 sftpGetFiles(connection, files$fileName, localFolder = localFolder)
 
@@ -23,6 +26,58 @@ sftpRm(connection, files$fileName)
 sftpDisconnect(connection)
 
 
-# Upload results to database -----------------------------------------------------------------------
+# Test results locally -----------------------------------------------------------------------------------
+CohortDiagnostics::preMergeDiagnosticsFiles(localFolder)
+CohortDiagnostics::launchDiagnosticsExplorer(localFolder)
 
-# TODO
+# Upload results to database -----------------------------------------------------------------------
+library(DatabaseConnector)
+connectionDetails <- createConnectionDetails(dbms = "postgresql",
+                                             server = paste(Sys.getenv("phenotypeLibraryDbServer"),
+                                                            Sys.getenv("phenotypeLibraryDbDatabase"),
+                                                            sep = "/"),
+                                             port = Sys.getenv("phenotypeLibraryDbPort"),
+                                             user = Sys.getenv("phenotypeLibraryDbUser"),
+                                             password = Sys.getenv("phenotypeLibraryDbPassword"))
+resultsSchema <- Sys.getenv("phenotypeLibraryDbResultsSchema")
+
+# Only the first time:
+# CohortDiagnostics::createResultsDataModel(connectionDetails = connectionDetails, schema = resultsSchema)
+
+uploadedFolder <- file.path(localFolder, "uploaded")
+if (!file.exists(uploadedFolder)) {
+  dir.create(uploadedFolder)
+}
+zipFilesToUpload <- list.files(path = localFolder, 
+                               pattern = ".zip", 
+                               recursive = FALSE)
+
+for (i in (1:length(zipFilesToUpload))) {
+  CohortDiagnostics:: uploadResults(connectionDetails = connectionDetails,
+                schema = resultsSchema,
+                zipFileName = file.path(localFolder, zipFilesToUpload[i]))
+  # Move to uploaded folder:
+  file.rename(file.path(localFolder, zipFilesToUpload[i]), file.path(uploadedFolder, zipFilesToUpload[i]))
+}
+
+# Only after the first time:
+# CohortDiagnostics::uploadPrintFriendly(connectionDetails = connectionDetails,
+#                                        schema = resultsSchema)
+
+# Grant rights to all:
+# connection <- connect(connectionDetails)
+# specs <- CohortDiagnostics::getResultsDataModelSpecifications()
+# tablesInSpecs <- c(unique(specs$tableName), "cohort_extra")
+# tablesOnServer <- getTableNames(connection, "results")
+# myTables <-  tablesInSpecs[tablesInSpecs%in% tolower(tablesOnServer)]
+# for (table in myTables) {
+#   print(table)
+#   sql <- sprintf("GRANT ALL PRIVILEGES ON %s.%s TO rw_grp;", "results", table)
+#   sql <- sprintf("GRANT ALL PRIVILEGES ON %s.%s TO phoebe;", "results", table)
+#   executeSql(connection, sql, progressBar = FALSE, reportOverallTime = FALSE)
+# }
+# disconnect(connection)
+
+CohortDiagnostics::launchDiagnosticsExplorer(connectionDetails = connectionDetails,
+                                             resultsDatabaseSchema = resultsSchema,
+                                             vocabularyDatabaseSchema = "vocabulary")
