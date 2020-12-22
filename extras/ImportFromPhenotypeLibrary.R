143239fc0ae07d8ab0ab0343add05f356378266b
Gowtham Rao <gowthamrao@gmail.com>
2020-12-21 19:51:37 -0500

    Updated with new cohort and phenotype definitions

--- extras/ImportFromPhenotypeLibrary.R
+++ extras/ImportFromPhenotypeLibrary.R
@@ -1,35 +1,74 @@
+library(magrittr)
 importFromPhenotypeLibrary <- function(phenotypeLibraryFolder = "../PhenotypeLibrary") {
-  phenotypeFolders <- list.files(file.path(phenotypeLibraryFolder, "inst"), pattern = "[0-9]+", full.names = TRUE)
-  
+  phenotypeFolders <- list.files(file.path(phenotypeLibraryFolder,
+                                           "inst"), pattern = "[0-9]+", full.names = TRUE)
+
   ParallelLogger::logInfo("Importing from ", length(phenotypeFolders), " phenotype folders")
-  
+
   ParallelLogger::logInfo("- Importing phenotype descriptions to 'inst/settings/phenotypeDescription.csv'")
-  phenotypeDescription <- lapply(file.path(phenotypeFolders, "phenotypeDescription.csv"), readr::read_csv, col_types = readr::cols())
+
+  # its possible for a phenotype to have no cohorts
+  phenotypeDescription <- list()
+  for (i in (1:length(phenotypeFolders))) {
+    description <- readr::read_csv(file = file.path(phenotypeFolders[[i]],
+                                                    "phenotypeDescription.csv"),
+                                   col_types = readr::cols())
+    if (nrow(description) > 0) {
+      phenotypeDescription[[i]] <- description
+    }
+  }
   phenotypeDescription <- dplyr::bind_rows(phenotypeDescription)
-  readr::write_csv(phenotypeDescription, "inst/settings/phenotypeDescription.csv")
-  
+  unlink("inst/settings/phenotypeDescription.csv")
+  dir.create(path = "inst/settings", showWarnings = FALSE, recursive = TRUE)
+  readr::write_excel_csv(x = phenotypeDescription,
+                         file = "inst/settings/phenotypeDescription.csv",
+                         na = "")
+
   ParallelLogger::logInfo("- Importing cohort descriptions to 'inst/settings/cohortDescription.csv'")
-  cohortDescription <- lapply(file.path(phenotypeFolders, "cohortDescription.csv"), readr::read_csv, col_types = readr::cols())
+  cohortDescription <- list()
+  for (i in (1:length(phenotypeFolders))) {
+    description <- readr::read_csv(file = file.path(phenotypeFolders[[i]], "cohortDescription.csv"),
+                                   col_types = readr::cols())
+    if (nrow(description) > 0) {
+      cohortDescription[[i]] <- description
+    }
+  }
   cohortDescription <- dplyr::bind_rows(cohortDescription)
-  readr::write_csv(cohortDescription, "inst/settings/cohortDescription.csv")
+  unlink("inst/settings/cohortDescription.csv")
+  readr::write_excel_csv(cohortDescription, file = "inst/settings/cohortDescription.csv", na = "")
+
+  # readr::write_excel_csv(x = phenotypeDescription %>% dplyr::inner_join(cohortDescription %>%
+  # dplyr::select(phenotypeId) %>% dplyr::distinct()), file = 'inst/settings/phenotypeDescription.csv',
+  # na = '')
+
+  # readr::write_excel_csv(cohortDescription %>% dplyr::inner_join(phenotypeDescription %>%
+  # dplyr::select(phenotypeId) %>% dplyr::distinct()), file = 'inst/settings/cohortDescription.csv', na
+  # = '')
+
   ParallelLogger::logInfo("- Found ", nrow(cohortDescription), " cohort descriptions")
-  
+
   # JSON
   ParallelLogger::logInfo("- Importing cohort JSON files to 'inst/cohorts'")
   unlink("inst/cohorts", recursive = TRUE)
   dir.create("inst/cohorts", recursive = TRUE)
-  jsonFiles <- list.files(file.path(phenotypeLibraryFolder, "inst"), pattern = "[0-9]+.json", recursive = TRUE, full.names = TRUE)
+  jsonFiles <- list.files(file.path(phenotypeLibraryFolder, "inst"),
+                          pattern = "[0-9]+.json",
+                          recursive = TRUE,
+                          full.names = TRUE)
   jsonFiles <- jsonFiles[grepl("[0-9]+/[0-9]+.json$", jsonFiles)]
   success <- all(file.copy(jsonFiles, file.path("inst/cohorts", basename(jsonFiles))))
   if (!success) {
     stop("Error copying JSON files")
   }
-  
+
   # SQL
   ParallelLogger::logInfo("- Importing cohort SQL files to 'inst/sql/sql_server'")
   unlink("inst/sql/sql_server", recursive = TRUE)
   dir.create("inst/sql/sql_server", recursive = TRUE)
-  sqlFiles <- list.files(file.path(phenotypeLibraryFolder, "inst"), pattern = "[0-9]+.sql", recursive = TRUE, full.names = TRUE)
+  sqlFiles <- list.files(file.path(phenotypeLibraryFolder, "inst"),
+                         pattern = "[0-9]+.sql",
+                         recursive = TRUE,
+                         full.names = TRUE)
   sqlFiles <- sqlFiles[grepl("[0-9]+/[0-9]+.sql$", sqlFiles)]
   success <- all(file.copy(sqlFiles, file.path("inst/sql/sql_server", basename(sqlFiles))))
   if (!success) {
@@ -38,17 +77,23 @@ importFromPhenotypeLibrary <- function(phenotypeLibraryFolder = "../PhenotypeLib
 
   ParallelLogger::logInfo("- Checking referential integrity")
   if (!setequal(phenotypeDescription$phenotypeId, unique(cohortDescription$phenotypeId))) {
-    stop("Phenotype ID mismatch")
+    mismatch <- setdiff(phenotypeDescription$phenotypeId, unique(cohortDescription$phenotypeId))
+    warning(paste0("Phenotype ID mismatch. The following mismatched ",
+                   paste0(mismatch, collapse = ",")))
   }
-  
+
   jsonCohortIds <- as.numeric(gsub(".json", "", basename(jsonFiles)))
   if (!setequal(jsonCohortIds, cohortDescription$cohortId)) {
-    stop("Cohort ID mistmatch for JSON files")
+    mismatch2 <- setdiff(jsonCohortIds, cohortDescription$cohortId)
+    warning("Cohort ID mistmatch for JSON files. The following mismatched ",
+            paste0(mismatch2, collapse = ","))
   }
-  
+
   sqlCohortIds <- as.numeric(gsub(".sql", "", basename(sqlFiles)))
   if (!setequal(sqlCohortIds, cohortDescription$cohortId)) {
-    stop("Cohort ID mistmatch for SQL files")
+    mismatch3 <- setdiff(sqlCohortIds, cohortDescription$cohortId)
+    warning("Cohort ID mistmatch for SQL files. The following mismatched ",
+            paste0(mismatch3, collapse = ","))
   }
   ParallelLogger::logInfo("Done importing")
   invisible(NULL)
--- inst/sql/sql_server/4201096004.sql
+++ inst/sql/sql_server/4201096004.sql
@@ -5,13 +5,24 @@ CREATE TABLE #Codesets (
 ;
 
 INSERT INTO #Codesets (codeset_id, concept_id)
-SELECT 0 as codeset_id, c.concept_id FROM (select distinct I.concept_id FROM
+SELECT 1 as codeset_id, c.concept_id FROM (select distinct I.concept_id FROM
 ( 
-  select concept_id from @vocabulary_database_schema.CONCEPT where concept_id in (321588)
+  select concept_id from @vocabulary_database_schema.CONCEPT where concept_id in (262,9201)
 UNION  select c.concept_id
   from @vocabulary_database_schema.CONCEPT c
   join @vocabulary_database_schema.CONCEPT_ANCESTOR ca on c.concept_id = ca.descendant_concept_id
-  and ca.ancestor_concept_id in (321588)
+  and ca.ancestor_concept_id in (262,9201)
+  and c.invalid_reason is null
+
+) I
+) C UNION ALL 
+SELECT 2 as codeset_id, c.concept_id FROM (select distinct I.concept_id FROM
+( 
+  select concept_id from @vocabulary_database_schema.CONCEPT where concept_id in (372615,378143,4147498,373189,4190307,379792,4330496,138965)
+UNION  select c.concept_id
+  from @vocabulary_database_schema.CONCEPT c
+  join @vocabulary_database_schema.CONCEPT_ANCESTOR ca on c.concept_id = ca.descendant_concept_id
+  and ca.ancestor_concept_id in (372615,4147498,373189,379792,4330496)
   and c.invalid_reason is null
 
 ) I
@@ -31,23 +42,22 @@ FROM
   (
   -- Begin Condition Occurrence Criteria
 SELECT C.person_id, C.condition_occurrence_id as event_id, C.condition_start_date as start_date, COALESCE(C.condition_end_date, DATEADD(day,1,C.condition_start_date)) as end_date,
-       C.CONDITION_CONCEPT_ID as TARGET_CONCEPT_ID, C.visit_occurrence_id,
-       C.condition_start_date as sort_date
+  C.visit_occurrence_id, C.condition_start_date as sort_date
 FROM 
 (
-  SELECT co.* , row_number() over (PARTITION BY co.person_id ORDER BY co.condition_start_date, co.condition_occurrence_id) as ordinal
+  SELECT co.* 
   FROM @cdm_database_schema.CONDITION_OCCURRENCE co
-  JOIN #Codesets codesets on ((co.condition_concept_id = codesets.concept_id and codesets.codeset_id = 0))
+  JOIN #Codesets codesets on ((co.condition_concept_id = codesets.concept_id and codesets.codeset_id = 2))
 ) C
 
-WHERE C.ordinal = 1
+
 -- End Condition Occurrence Criteria
 
   ) E
 	JOIN @cdm_database_schema.observation_period OP on E.person_id = OP.person_id and E.start_date >=  OP.observation_period_start_date and E.start_date <= op.observation_period_end_date
   WHERE DATEADD(day,0,OP.OBSERVATION_PERIOD_START_DATE) <= E.START_DATE AND DATEADD(day,0,E.START_DATE) <= OP.OBSERVATION_PERIOD_END_DATE
 ) P
-WHERE P.ordinal = 1
+
 -- End Primary Events
 
 )
@@ -58,16 +68,180 @@ FROM
   select pe.event_id, pe.person_id, pe.start_date, pe.end_date, pe.op_start_date, pe.op_end_date, row_number() over (partition by pe.person_id order by pe.start_date ASC) as ordinal, cast(pe.visit_occurrence_id as bigint) as visit_occurrence_id
   FROM primary_events pe
   
+JOIN (
+-- Begin Criteria Group
+select 0 as index_id, person_id, event_id
+FROM
+(
+  select E.person_id, E.event_id 
+  FROM primary_events E
+  INNER JOIN
+  (
+    -- Begin Correlated Criteria
+select 0 as index_id, cc.person_id, cc.event_id
+from (SELECT p.person_id, p.event_id 
+FROM primary_events P
+JOIN (
+  -- Begin Visit Occurrence Criteria
+select C.person_id, C.visit_occurrence_id as event_id, C.visit_start_date as start_date, C.visit_end_date as end_date,
+       C.visit_occurrence_id, C.visit_start_date as sort_date
+from 
+(
+  select vo.* 
+  FROM @cdm_database_schema.VISIT_OCCURRENCE vo
+JOIN #Codesets codesets on ((vo.visit_concept_id = codesets.concept_id and codesets.codeset_id = 1))
+) C
+
+
+-- End Visit Occurrence Criteria
+
+) A on A.person_id = P.person_id  AND A.START_DATE >= P.OP_START_DATE AND A.START_DATE <= P.OP_END_DATE AND A.START_DATE >= P.OP_START_DATE AND A.START_DATE <= DATEADD(day,0,P.START_DATE) AND A.END_DATE >= DATEADD(day,0,P.START_DATE) AND A.END_DATE <= P.OP_END_DATE ) cc 
+GROUP BY cc.person_id, cc.event_id
+HAVING COUNT(cc.event_id) >= 1
+-- End Correlated Criteria
+
+  ) CQ on E.person_id = CQ.person_id and E.event_id = CQ.event_id
+  GROUP BY E.person_id, E.event_id
+  HAVING COUNT(index_id) = 1
+) G
+-- End Criteria Group
+) AC on AC.person_id = pe.person_id and AC.event_id = pe.event_id
+
 ) QE
 
 ;
 
 --- Inclusion Rule Inserts
 
-create table #inclusion_events (inclusion_rule_id bigint,
-	person_id bigint,
-	event_id bigint
-);
+select 0 as inclusion_rule_id, person_id, event_id
+INTO #Inclusion_0
+FROM 
+(
+  select pe.person_id, pe.event_id
+  FROM #qualified_events pe
+  
+JOIN (
+-- Begin Criteria Group
+select 0 as index_id, person_id, event_id
+FROM
+(
+  select E.person_id, E.event_id 
+  FROM #qualified_events E
+  INNER JOIN
+  (
+    -- Begin Correlated Criteria
+select 0 as index_id, p.person_id, p.event_id
+from #qualified_events p
+LEFT JOIN (
+SELECT p.person_id, p.event_id 
+FROM #qualified_events P
+JOIN (
+  select PE.person_id, PE.event_id, PE.start_date, PE.end_date, PE.visit_occurrence_id, PE.sort_date FROM (
+-- Begin Condition Occurrence Criteria
+SELECT C.person_id, C.condition_occurrence_id as event_id, C.condition_start_date as start_date, COALESCE(C.condition_end_date, DATEADD(day,1,C.condition_start_date)) as end_date,
+  C.visit_occurrence_id, C.condition_start_date as sort_date
+FROM 
+(
+  SELECT co.* 
+  FROM @cdm_database_schema.CONDITION_OCCURRENCE co
+  JOIN #Codesets codesets on ((co.condition_concept_id = codesets.concept_id and codesets.codeset_id = 2))
+) C
+
+
+-- End Condition Occurrence Criteria
+
+) PE
+JOIN (
+-- Begin Criteria Group
+select 0 as index_id, person_id, event_id
+FROM
+(
+  select E.person_id, E.event_id 
+  FROM (SELECT Q.person_id, Q.event_id, Q.start_date, Q.end_date, Q.visit_occurrence_id, OP.observation_period_start_date as op_start_date, OP.observation_period_end_date as op_end_date
+FROM (-- Begin Condition Occurrence Criteria
+SELECT C.person_id, C.condition_occurrence_id as event_id, C.condition_start_date as start_date, COALESCE(C.condition_end_date, DATEADD(day,1,C.condition_start_date)) as end_date,
+  C.visit_occurrence_id, C.condition_start_date as sort_date
+FROM 
+(
+  SELECT co.* 
+  FROM @cdm_database_schema.CONDITION_OCCURRENCE co
+  JOIN #Codesets codesets on ((co.condition_concept_id = codesets.concept_id and codesets.codeset_id = 2))
+) C
+
+
+-- End Condition Occurrence Criteria
+) Q
+JOIN @cdm_database_schema.OBSERVATION_PERIOD OP on Q.person_id = OP.person_id 
+  and OP.observation_period_start_date <= Q.start_date and OP.observation_period_end_date >= Q.start_date
+) E
+  INNER JOIN
+  (
+    -- Begin Correlated Criteria
+select 0 as index_id, cc.person_id, cc.event_id
+from (SELECT p.person_id, p.event_id 
+FROM (SELECT Q.person_id, Q.event_id, Q.start_date, Q.end_date, Q.visit_occurrence_id, OP.observation_period_start_date as op_start_date, OP.observation_period_end_date as op_end_date
+FROM (-- Begin Condition Occurrence Criteria
+SELECT C.person_id, C.condition_occurrence_id as event_id, C.condition_start_date as start_date, COALESCE(C.condition_end_date, DATEADD(day,1,C.condition_start_date)) as end_date,
+  C.visit_occurrence_id, C.condition_start_date as sort_date
+FROM 
+(
+  SELECT co.* 
+  FROM @cdm_database_schema.CONDITION_OCCURRENCE co
+  JOIN #Codesets codesets on ((co.condition_concept_id = codesets.concept_id and codesets.codeset_id = 2))
+) C
+
+
+-- End Condition Occurrence Criteria
+) Q
+JOIN @cdm_database_schema.OBSERVATION_PERIOD OP on Q.person_id = OP.person_id 
+  and OP.observation_period_start_date <= Q.start_date and OP.observation_period_end_date >= Q.start_date
+) P
+JOIN (
+  -- Begin Visit Occurrence Criteria
+select C.person_id, C.visit_occurrence_id as event_id, C.visit_start_date as start_date, C.visit_end_date as end_date,
+       C.visit_occurrence_id, C.visit_start_date as sort_date
+from 
+(
+  select vo.* 
+  FROM @cdm_database_schema.VISIT_OCCURRENCE vo
+JOIN #Codesets codesets on ((vo.visit_concept_id = codesets.concept_id and codesets.codeset_id = 1))
+) C
+
+
+-- End Visit Occurrence Criteria
+
+) A on A.person_id = P.person_id  AND A.START_DATE >= P.OP_START_DATE AND A.START_DATE <= P.OP_END_DATE AND A.START_DATE >= P.OP_START_DATE AND A.START_DATE <= DATEADD(day,0,P.START_DATE) AND A.END_DATE >= DATEADD(day,0,P.START_DATE) AND A.END_DATE <= P.OP_END_DATE ) cc 
+GROUP BY cc.person_id, cc.event_id
+HAVING COUNT(cc.event_id) >= 1
+-- End Correlated Criteria
+
+  ) CQ on E.person_id = CQ.person_id and E.event_id = CQ.event_id
+  GROUP BY E.person_id, E.event_id
+  HAVING COUNT(index_id) = 1
+) G
+-- End Criteria Group
+) AC on AC.person_id = pe.person_id and AC.event_id = pe.event_id
+
+) A on A.person_id = P.person_id  AND A.START_DATE >= DATEADD(day,-183,P.START_DATE) AND A.START_DATE <= DATEADD(day,-1,P.START_DATE) ) cc on p.person_id = cc.person_id and p.event_id = cc.event_id
+GROUP BY p.person_id, p.event_id
+HAVING COUNT(cc.event_id) = 0
+-- End Correlated Criteria
+
+  ) CQ on E.person_id = CQ.person_id and E.event_id = CQ.event_id
+  GROUP BY E.person_id, E.event_id
+  HAVING COUNT(index_id) = 1
+) G
+-- End Criteria Group
+) AC on AC.person_id = pe.person_id AND AC.event_id = pe.event_id
+) Results
+;
+
+SELECT inclusion_rule_id, person_id, event_id
+INTO #inclusion_events
+FROM (select inclusion_rule_id, person_id, event_id from #Inclusion_0) I;
+TRUNCATE TABLE #Inclusion_0;
+DROP TABLE #Inclusion_0;
+
 
 with cteIncludedEvents(event_id, person_id, start_date, end_date, op_start_date, op_end_date, ordinal) as
 (
@@ -80,21 +254,31 @@ with cteIncludedEvents(event_id, person_id, start_date, end_date, op_start_date,
     GROUP BY Q.event_id, Q.person_id, Q.start_date, Q.end_date, Q.op_start_date, Q.op_end_date
   ) MG -- matching groups
 
+  -- the matching group with all bits set ( POWER(2,# of inclusion rules) - 1 = inclusion_rule_mask
+  WHERE (MG.inclusion_rule_mask = POWER(cast(2 as bigint),1)-1)
+
 )
 select event_id, person_id, start_date, end_date, op_start_date, op_end_date
 into #included_events
 FROM cteIncludedEvents Results
-WHERE Results.ordinal = 1
+
 ;
 
+-- date offset strategy
+
+select event_id, person_id, 
+  case when DATEADD(day,1,start_date) > op_end_date then op_end_date else DATEADD(day,1,start_date) end as end_date
+INTO #strategy_ends
+from #included_events;
 
 
 -- generate cohort periods into #final_cohort
 with cohort_ends (event_id, person_id, end_date) as
 (
 	-- cohort exit dates
-  -- By default, cohort exit at the event's op end date
-select event_id, person_id, op_end_date as end_date from #included_events
+  -- End Date Strategy
+SELECT event_id, person_id, end_date from #strategy_ends
+
 ),
 first_ends (person_id, start_date, end_date) as
 (
@@ -297,6 +481,8 @@ DROP TABLE #best_events;
 
 
 
+TRUNCATE TABLE #strategy_ends;
+DROP TABLE #strategy_ends;
 
 
 TRUNCATE TABLE #cohort_rows;
