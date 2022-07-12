ae1c8cf6aa4028c8346433ab07baf961b0fba56e
Gowtham Rao <rao@ohdsi.org>
2022-07-12 10:46:25 -0400

    Trying to add some tests
    * trying to add some tests
    
    * Update DESCRIPTION to order dependency

---
+++ tests/testthat/test-executePhenotyeLibraryDiagnostics.R
@@ -0,0 +1,7 @@
+library(testthat)
+testthat::test_that("Testing if package can execute", {
+  # generate unique name for a cohort table
+  sysTime <- as.numeric(Sys.time()) * 100000
+  tableName <- paste0("cr", sysTime)
+  tempTableName <- paste0("#", tableName, "_1")
+})
