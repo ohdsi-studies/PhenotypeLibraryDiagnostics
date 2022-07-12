ba8db6b647983c9bdf4ccd9db749656572703e5c
Gowtham Rao <rao@ohdsi.org>
2022-07-12 11:59:16 -0400

    Add verify dependencies

--- tests/testthat/test-executePhenotyeLibraryDiagnostics.R
+++ tests/testthat/test-executePhenotyeLibraryDiagnostics.R
@@ -4,4 +4,19 @@ testthat::test_that("Testing if package can execute", {
   sysTime <- as.numeric(Sys.time()) * 100000
   tableName <- paste0("cr", sysTime)
   tempTableName <- paste0("#", tableName, "_1")
+  
+  folder <- tempfile()
+  dir.create(folder, recursive = TRUE)
+  
+  connectionDetails <- Eunomia::getEunomiaConnectionDetails()
+  PhenotypeLibraryDiagnostics::executePhenotyeLibraryDiagnostics(
+      connectionDetails = connectionDetails, 
+      cdmDatabaseSchema = "main", 
+      vocabularyDatabaseSchema = "main", 
+      cohortDatabaseSchema = "main", 
+      cohortTable = tempTableName,
+      outputFolder = folder
+  )
+  
+  unlink(x = folder, recursive = TRUE, force = TRUE)
 })
